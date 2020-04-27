e2b3a8b01bd7071c3b269e3567805400324e81a6
williamlee52 <63024535+williamlee52@users.noreply.github.com>
2020-04-27 11:45:43 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,379 @@
+namespace SpriteKind {
+    export const Edge = SpriteKind.create()
+    export const ball = SpriteKind.create()
+    export const Top = SpriteKind.create()
+    export const Brick = SpriteKind.create()
+}
+function buildsetbricks () {
+    for (let index = 0; index <= 6; index++) {
+        for (let index2 = 0; index2 < 4; index2++) {
+            createbrick(index * 16 + 32, col * 8 + 24)
+            col += 1
+        }
+        col = 0
+    }
+}
+sprites.onOverlap(SpriteKind.ball, SpriteKind.Brick, function (sprite, otherSprite) {
+    info.changeScoreBy(15)
+    otherSprite.destroy(effects.ashes, 200)
+    sprite.setVelocity(sprite.vx, -1 * sprite.vy)
+    numbBricks += -1
+})
+function createbrick (x: number, y: number) {
+    randnum = Math.randomRange(0, 2)
+    if (randnum == 0) {
+        brick2 = sprites.create(img`
+f f f f f f f f f f f f f f f f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f f f f f f f f f f f f f f f f 
+`, SpriteKind.Brick)
+    } else if (randnum == 1) {
+        brick2 = sprites.create(img`
+f f f f f f f f f f f f f f f f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f f f f f f f f f f f f f f f f 
+`, SpriteKind.Brick)
+    } else {
+        brick2 = sprites.create(img`
+f f f f f f f f f f f f f f f f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+f f f f f f f f f f f f f f f f 
+`, SpriteKind.Brick)
+    }
+    brick2.setPosition(x, y)
+    numbBricks += 1
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    console.log(convertToText(numbBricks))
+})
+sprites.onOverlap(SpriteKind.ball, SpriteKind.Edge, function (sprite, otherSprite) {
+    sprite.setVelocity(-1 * sprite.vx, sprite.vy)
+})
+sprites.onOverlap(SpriteKind.ball, SpriteKind.Player, function (sprite, otherSprite) {
+    sprite.setVelocity((sprite.x - otherSprite.x) * 3, -1 * sprite.vy)
+    if (sprite.vy > -150) {
+        sprite.vy += -5
+    }
+})
+sprites.onOverlap(SpriteKind.ball, SpriteKind.Top, function (sprite, otherSprite) {
+    sprite.setVelocity(sprite.vx, -1 * sprite.vy)
+})
+let brick2: Sprite = null
+let randnum = 0
+let col = 0
+let numbBricks = 0
+info.setScore(0)
+info.setLife(3)
+let startballvar = 0
+scene.setBackgroundColor(8)
+let paddle = sprites.create(img`
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+`, SpriteKind.Player)
+paddle.setPosition(80, 110)
+controller.moveSprite(paddle, 100, 0)
+paddle.setFlag(SpriteFlag.StayInScreen, true)
+let top = sprites.create(img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`, SpriteKind.Top)
+top.setPosition(80, 0)
+let left = sprites.create(img`
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+`, SpriteKind.Edge)
+left.setPosition(0, 60)
+let right = sprites.create(img`
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+1 
+`, SpriteKind.Edge)
+right.setPosition(159, 60)
+let ballvar = sprites.create(img`
+. 1 1 . 
+1 1 1 1 
+1 1 1 1 
+. 1 1 . 
+`, SpriteKind.ball)
+numbBricks = 0
+col = 0
+buildsetbricks()
+game.onUpdate(function () {
+    if (startballvar == 0) {
+        ballvar.setPosition(paddle.x, 105)
+        ballvar.setVelocity(0, 0)
+        if (controller.A.isPressed()) {
+            startballvar = 1
+        }
+    }
+    if (startballvar == 1) {
+        ballvar.setVelocity(Math.randomRange(-30, 30), -50)
+        startballvar = 2
+    }
+    if (ballvar.y > 115) {
+        startballvar = 0
+        info.changeLifeBy(-1)
+    }
+})
+forever(function () {
+    if (numbBricks <= 0) {
+        numbBricks = 0
+        startballvar = 0
+        effects.confetti.startScreenEffect()
+        pause(2000)
+        effects.confetti.endScreenEffect()
+        buildsetbricks()
+        info.changeScoreBy(100)
+    }
+})
